f643f98aa2a95c1655fe182bcbf85ac065899fac
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2020-06-11 17:13:44 -0700

    Added Lengths controller.

---
+++ Controllers/LengthsController.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Unit_Conversions_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LengthsController : ControllerBase
+    {
+        
+    }
+}
